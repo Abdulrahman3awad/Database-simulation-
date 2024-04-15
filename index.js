e0199ac4fffc9e67d6176e5a6a7d34d06c49d2f7
Abdulrahman Awad <abdh.3wad@gmail.com>
2024-04-15 07:10:27 +0200

    find and create methods

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-const { error, log } = require('console');
+const { error } = require('console');
+const { uid } = require('uid/single');
 let fs = require('fs')
 const db = {
-    dataJs:{},
     find(col, qry) {
-        let result = []
-        for (const obj of this.dataJs[col]) {
-            if (obj[JSON.stringify(qry).slice(1,-1).split(":")[0].slice(1,-1)] == JSON.stringify(qry).slice(1,-1).split(":")[1].slice(1,-1) ) {
-                result.push(obj)
+        try {
+            let data = fs.readFileSync("database.json", "utf8");
+            let result = [];
+            for (const obj of JSON.parse(data)[col]) {
+                if (obj[JSON.stringify(qry).slice(1, -1).split(":")[0].slice(1, -1)] == JSON.stringify(qry).slice(1, -1).split(":")[1].slice(1, -1)) {
+                    result.push(obj);
+                }
             }
-            else continue
+            return result;
+        } catch (err) {
+            console.error(err);
+            return null;
         }
-        return result
     },
     create(col, doc) {
-        // ...
+        fs.readFile("database.json" ,"utf8", (err, data)=>{
+        if (err) {
+            console.error(err);
+            return
+        }
+        const dataJs = JSON.parse(data);
+        dataJs[col].push(...doc);
+        fs.writeFile("database.json", JSON.stringify(dataJs), (err)=>{
+            if (err) {
+                console.error(err);
+                return
+            }
+            else console.log("The file has been saved!");
+        })
+    })
     },
     update(col, qry, upd) {
         // ...
@@ -22,11 +41,8 @@ const db = {
         // ...
     },
 }
-fs.readFile("database.json" ,"utf8", (err, data)=>{
-    if (err) {
-        console.error(err);
-        return
-    }
-    db.dataJs = JSON.parse(data); 
-    console.log(db.find("users", {"age":"50"}));
-})
+console.log(db.find("users", {"age":"50"}));
+console.log(db.create("users", [
+	{"id":uid(),"title":"Learn NodeJS","pages":422,"language":"English","author":"Ahmed Zanaty","category":"Technology"},
+	{"id":uid(),"title":"Learn NodeJS","pages":422,"language":"English","author":"Ahmed Zanaty","category":"Technology"}
+]));
